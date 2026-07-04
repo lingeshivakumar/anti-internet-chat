353f397881105eef228720b52f188b2324367ae9
Lingesh Shivakumar <lingesh221204@gmail.com>
2026-07-04 22:22:37 +0530

    initializing project structure

---
+++ client/src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1><b>A.I. Chat / Anti-Internet Chat</b></h1>
+      <p>Communicate when the internet can't.</p>
+    </div>
+  );
+}
+
+export default App;

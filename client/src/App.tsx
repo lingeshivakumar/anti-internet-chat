dea08e548b767c2cb0335f037f81acd37e27dd80
Lingesh Shivakumar <lingesh221204@gmail.com>
2026-07-05 11:46:38 +0530

    configured tailwind css and updated the landing page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,20 @@
 function App() {
   return (
-    <div>
-      <h1><b>A.I. Chat / Anti-Internet Chat</b></h1>
-      <p>Communicate when the internet can't.</p>
-    </div>
+    <main className="min-h-screen bg-white-950 text-white flex flex-col items-center justify-center">
+      <h1 className="text-5xl text-black font-bold mb-4">
+        Anti-Internet Chat
+      </h1>
+
+      <p className="text-slate-400 text-lg">
+        Communicate when the internet can't
+      </p>
+
+      <button
+        className="mt-8 rounded-full bg-blue-600 px-6 py-3 font-semibold transition hover:bg-blue-500"
+      >
+        Get Started
+      </button>
+    </main>
   );
 }
 

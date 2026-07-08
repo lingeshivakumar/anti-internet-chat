e85c3c2a6d8d46ea04a835b0d7ea579b791ac6e7
Lingesh Shivakumar <lingesh221204@gmail.com>
2026-07-08 21:51:10 +0530

    updated home.tsx

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -6,8 +6,8 @@ export default function Home() {
     <main className="min-h-screen bg-[#0A0C10] p-5">
       <div className="grid h-[calc(100vh-40px)] grid-cols-[290px_1fr] gap-5">
         <Sidebar
-          userName="Lingesh"
-          deviceName="MacBook Air M2"
+          userName="User"
+          deviceName="Your devices"
         />
 
         <ChatWindow />

5c2276b751005be8021c270cab204ae42e791c54
Lingesh Shivakumar <lingesh221204@gmail.com>
2026-07-19 09:51:36 +0530

    integrates session identity flow into app upon startup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,30 @@
 import { useEffect } from "react";
+
 import Home from "./pages/Home";
+import NameModal from "./components/NameModal";
+
 import { socket } from "./services/socket";
+import { useIdentity } from "./hooks/useIdentity";
 
 export default function App() {
+  const {
+    hasIdentity,
+    createIdentity,
+  } = useIdentity();
 
   useEffect(() => {
-
     socket.connect();
 
     return () => {
       socket.disconnect();
     };
-
   }, []);
 
-  return <Home />;
-
+  return hasIdentity ? (
+    <Home />
+  ) : (
+    <NameModal
+      onContinue={createIdentity}
+    />
+  );
 }
